7eca1d7e923897932a850880af756683b9e685d4
NiiJoshua <joshkobeng@gmail.com>
2021-08-12 14:16:33 +0000

    update

--- sentiment.py
+++ sentiment.py
@@ -14,3 +14,36 @@ access_token_secret = "xxxx"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
+
+for tweet in api.search('username'): # replace the search parameter with twitter username
+    print(tweet.text)
+
+# Creating a dataframe of the columns we want to extract from Twirter
+df = pd.DataFrame(columns = ['Tweets', 'User', 'User_statuses_count', 
+                             'user_followers', 'User_location', 'User_verified',
+                             'fav_count', 'rt_count', 'tweet_date'])
+
+# Defind a function to scrape the data
+def stream(data, file_name):
+    i = 0
+    for tweet in tweepy.Cursor(api.search, q=data, count=100, lang='en').items():
+        print(i, end='\r')
+        df.loc[i, 'Tweets'] = tweet.text
+        df.loc[i, 'User'] = tweet.user.name
+        df.loc[i, 'User_statuses_count'] = tweet.user.statuses_count
+        df.loc[i, 'user_followers'] = tweet.user.followers_count
+        df.loc[i, 'User_location'] = tweet.user.location
+        df.loc[i, 'User_verified'] = tweet.user.verified
+        df.loc[i, 'fav_count'] = tweet.favorite_count
+        df.loc[i, 'rt_count'] = tweet.retweet_count
+        df.loc[i, 'tweet_date'] = tweet.created_at
+        df.to_excel('{}.xlsx'.format(file_name))
+        i+=1
+        if i == 3200:
+            break
+        else:
+            pass
+
+df.head()
+
+# Working the sentiment Analysis part
